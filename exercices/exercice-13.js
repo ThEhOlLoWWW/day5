f0dbfa6a49cfe5ff3e5674259696c36d6fc3dc91
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 11:27:44 +0100

    ex13 done

--- exercices/exercice-13.js
+++ exercices/exercice-13.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function formater(montant){
+   
+    let num = montant.toLocaleString("fr-FR");
+    return `${num} MAD`
+}
+console.log(formater(1234567));
+
