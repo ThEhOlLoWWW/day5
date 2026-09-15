33342a7690c352cd9259c1bf377b0c7e5873d56f
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 09:25:13 +0100

    ex8 done

--- exercices/exercice-08.js
+++ exercices/exercice-08.js
@@ -14,4 +14,13 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function capitaliser(mot){
+    let mot1 = mot.toLowerCase();
+    return mot1[0].toUpperCase() + mot1.slice(1)
+}
+
+let m = "yOUcoDe"
+console.log(capitaliser(m));
+
+
