6943c6326f1f9002f335a7b7754b0f2157382050
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 09:12:50 +0100

    ex6 done

--- exercices/exercice-06.js
+++ exercices/exercice-06.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const chaîne = "Safi,Youssoufia,Nador,Casablanca" 
+
+const array = chaîne.split(",")
+
+console.log(array);
