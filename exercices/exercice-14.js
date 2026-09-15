16b846f901f64cc9bc52dba38395fa5467ea958b
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 12:00:57 +0100

    ex14 done

--- exercices/exercice-14.js
+++ exercices/exercice-14.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function Comptez(chaîne){
+    let array = chaîne.trim();
+    array = array.split(" ");
+    array = array.filter(item => item !== "")
+    return array.length;
+}
+
+console.log(Comptez("  hello  my name  is Markeplier"));
