97d99e2ad52aab0df08352b4bb3f6f2b46f1908b
ThEhOlLoWWW <dryounes2005@gmail.com>
2026-09-15 09:39:56 +0100

    ex9 done

--- exercices/exercice-09.js
+++ exercices/exercice-09.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let phrase = "Ne sois pas idiot, réfléchis avant de parler."
+const mot = "idiot"
+
+phrase = phrase.replace(mot, "*****");
+
+console.log(phrase);
+
